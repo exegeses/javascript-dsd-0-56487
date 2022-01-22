eff4d628a263d3924336f9e8611f1b8ef483f709
Marcos <exegeses@gmail.com>
2022-01-21 22:03:38 -0300

    funciones con javascript

---
+++ clase3/js/funciones.js
@@ -0,0 +1,17 @@
+//ubicamos elemento dentro del DOM
+let caja = document.getElementById('caja');
+
+//modificamos atributos: altura, ancho, color de fondo
+/*
+caja.style.height = '150px';
+caja.style.width  = '95px';
+caja.style.backgroundColor = '#b0fce3';
+*/
+
+//declaramos función
+function modificarAtributos()
+{
+    caja.style.height = '150px';
+    caja.style.width  = '95px';
+    caja.style.backgroundColor = '#b0fce3';
+}
---
+++ clase3/js/mostrarOcultar.js
@@ -0,0 +1,14 @@
+//ubicar elemento dentro del DOM
+let caja = document.getElementById('caja');
+
+//declaramos funciones de control
+function ocultar()
+{
+    //display:none
+    caja.style.display = 'none';
+}
+function mostrar()
+{
+    //display:block
+    caja.style.display = 'block';
+}
